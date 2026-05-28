35a0d938d9e8197ac730f0a8b2fe8efbace9531a
Santy G <mynameissantiagogarcia@gmail.com>
2026-05-28 14:13:25 -0400

    added styling to the backtotop button

--- client/src/components/common/BackToTopButton.tsx
+++ client/src/components/common/BackToTopButton.tsx
@@ -1,21 +1,22 @@
-import style from "./BackToTopButton.module.css"
+import styles from "./BackToTopButton.module.css";
 
 type BackToTopProp = {
-    divRef: React.RefObject<HTMLDivElement | null>
-}
+    divRef: React.RefObject<HTMLDivElement | null>;
+};
 
-
-export default function BackToTop({ divRef, }: BackToTopProp) {
-
+export default function BackToTop({ divRef }: BackToTopProp) {
     return (
-        <>
-            <button onClick={() => {
+        <button
+            className={styles.button}
+            aria-label="Scroll to top"
+            onClick={() => {
                 divRef.current?.scrollTo({
-                    top: 0, behavior: "smooth"
-                })
-            }}>
-                Back To Top
-            </button>
-        </>
+                    top: 0,
+                    behavior: "smooth",
+                });
+            }}
+        >
+            ↑
+        </button>
     );
 }
